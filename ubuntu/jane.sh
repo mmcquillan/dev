0c48bb68383247b42215a4519c636f8380a403d1
Matt McQuillan <automatedmatt@gmail.com>
2016-03-17 14:43:15 -0400

    fixed jane script

--- ubuntu/jane.sh
+++ ubuntu/jane.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
+echo "Setting paths"
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=/tmp/go
 export GOBIN=$GOPATH/bin
-go get github.com/projectjane/jane
-go install $GOPATH/src/github.com/projectjane/jane/jane.go
-sudo cp $GOPATH/bin/jane /usr/local/bin/jane
-sudo cp $GOPATH/src/github.com/projectjane/jane/startup/upstart/jane.conf /etc/init/jane.conf
-rm -rf /tmp/go
-sudo service jane restart
 
+echo "Getting latest Jane"
+go get github.com/projectjane/jane
+
+echo "Building latest Jane"
+go install $GOPATH/src/github.com/projectjane/jane/jane.go
+
+echo "Stopping Jane"
+sudo service jane stop
+
+echo "Copying binaries"
+sudo cp $GOPATH/bin/jane /usr/local/bin/jane
+
+echo "Updating upstart script"
+sudo cp $GOPATH/src/github.com/projectjane/jane/startup/upstart/jane.conf /etc/init/jane.conf
+
+echo "Starting Jane"
+sudo service jane start
+
+echo "Cleaning up"
+rm -rf /tmp/go
